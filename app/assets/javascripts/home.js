a636249fd6e51210cbbce5f1cd897d3133988673
mligms <mligms@gmail.com>
2015-07-27 17:11:45 +0000

    fixed sliding header

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,10 +2,6 @@ $(window).scroll(function () {
   if ( $(this).scrollTop() > 500 && !$('.custom2').hasClass('open')) {
     $('.custom2').addClass('open');
     $('.custom2').slideDown();
-  } else if ( $(this).scrollTop() <= 500 ) {
-  if ( $(this).scrollTop() > 500 && !$('.custom2').hasClass('open') ) {
-    $('.custom2').addClass('open');
-    $('.custom2').slideDown();
    } else if ( $(this).scrollTop() <= 500 ) {
     $('.custom2').removeClass('open');
     $('.custom2').slideUp();
