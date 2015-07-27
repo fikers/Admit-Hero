d656f098beda1e5dc87b6553dc244d4ebc817161
Lejia Duan <lejia.duan@gmail.com>
2015-07-27 11:37:11 -0500

    prettify things

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,8 @@
 $(window).scroll(function () {
-  if ( $(this).scrollTop() > 500 && !$('.custom2').hasClass('open') ) {
+  if ( $(this).scrollTop() > 550 && !$('.custom2').hasClass('open') ) {
     $('.custom2').addClass('open');
     $('.custom2').slideDown();
-   } else if ( $(this).scrollTop() <= 500 ) {
+   } else if ( $(this).scrollTop() <= 550 ) {
     $('.custom2').removeClass('open');
     $('.custom2').slideUp();
   }
