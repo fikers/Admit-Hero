36da14dd3a441c21720d6c6a55101a1ef7fe801b
hancle <hancleyan@gmail.com>
2015-07-29 12:19:38 -0500

    last version of tag profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-	before_action :logged_in_user, except: [:new]
-	before_action :correct_user, except: [:new, :index, :destroy]
+	before_action :logged_in_user, except: [:new, :create]
+	before_action :correct_user, except: [:new, :create, :index, :destroy]
 	before_action :admin_user, only: [:index, :destroy]
 
 	def index
