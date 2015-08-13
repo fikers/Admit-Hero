5c5a02359cc6fe8fb7bdc97a91fad5c4c36d094c
hancle <hancleyan@gmail.com>
2015-08-13 16:10:50 -0500

    813 update

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,15 @@ class StaticPagesController < ApplicationController
 	def home 
 		if logged_in?
 			tags = {}
-			truetags = current_user.tag.select {|key, value| value == true}
-			truetags.each do |key, value|
-				usertag= TagLib.find_by(tag: key)
-				tags.push(usertag)
+			if current_user.tag
+				debugger
+				truetags = current_user.tag.select {|key, value| value == true}
+				truetags.each do |key, value|
+					usertag= TagLib.find_by(tag: key)
+					tags.push(usertag)
+				end
+				@feedtags = tags.paginate(page: 5)
 			end
-			@feedtags = tags.paginate(page: 5)
 		end
 	end
 
