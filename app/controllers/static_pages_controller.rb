146ad0916ddf70c821bb7623475c4ccdf42e0ccf
hancle <hancleyan@gmail.com>
2015-08-19 17:36:13 -0500

    select method works

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,11 @@
 class StaticPagesController < ApplicationController
 	def home 
 		if logged_in?
-			tags = {}
-			if current_user.tag
-				debugger
-				truetags = current_user.tag.select {|key, value| value == true}
-				truetags.each do |key, value|
-					usertag= TagLib.find_by(tag: key)
-					tags.push(usertag)
-				end
-				@feedtags = tags.paginate(page: 5)
+			@usertags = Array.new
+			truetags = current_user.tag.attributes.select{|key, value| value==true}
+			truetags.each do |key, value|
+				tag = TagLib.find_by(tag: key)
+				@usertags.push(tag)
 			end
 		end
 	end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
-  belongs_to :user
-  validates :user_id, presence: true
+	
+	belongs_to :user
+	validates :user_id, presence: true
+
 end
