5de6c7ad486b4a1ace008cb4b25808e171d075d8
hancle <hancleyan@gmail.com>
2015-08-20 09:14:36 -0500

    update 0820 8:42

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,16 @@
 class StaticPagesController < ApplicationController
 	def home 
 		if logged_in?
-			@usertags = Array.new
+			usertags = Array.new
 			@articles = Array.new
-			truetags = current_user.tag.attributes.select{|key, value| value==true}
+			truetags = current_user.tag.select_true_tags
 			truetags.each do |key, value|
 				tag = TagLib.find_by(tag: key)
-				@usertags.push(tag)
+				usertags.push(tag)
 			end
-			@usertags.each do |tag|
+			usertags.each do |tag|
 				tag.has_articles.each do |article|
-					@articles.push(article) if !@articles.include?(article)
+					@articles.push(article) unless @articles.include?(article)
 				end
 			end
 			@articles = @articles.sort_by{|hash| -hash['id']}
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,9 @@
 class Tag < ActiveRecord::Base
-	
 	belongs_to :user
 	validates :user_id, presence: true
+
+	def select_true_tags
+		attributes.select{|key, value| value==true}
+	end
 
 end
