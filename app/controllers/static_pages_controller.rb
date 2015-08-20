0016e9e629fa7501c824e41bbc58a35cb0d84f1f
hancle <hancleyan@gmail.com>
2015-08-20 00:39:23 -0500

    update feed sort

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,11 +2,18 @@ class StaticPagesController < ApplicationController
 	def home 
 		if logged_in?
 			@usertags = Array.new
+			@articles = Array.new
 			truetags = current_user.tag.attributes.select{|key, value| value==true}
 			truetags.each do |key, value|
 				tag = TagLib.find_by(tag: key)
 				@usertags.push(tag)
 			end
+			@usertags.each do |tag|
+				tag.has_articles.each do |article|
+					@articles.push(article) if !@articles.include?(article)
+				end
+			end
+			@articles = @articles.sort_by{|hash| -hash['id']}
 		end
 	end
 
